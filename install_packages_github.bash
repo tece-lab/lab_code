4c8e73e12a538a24de981bcb4ecdb1e08c0649b7
Giovanni Laudanno <glaudanno@gmail.com>
2019-01-31 18:34:43 +0100

    Added github and cran versions

---
+++ install_packages_github.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Code by Giovanni Laudanno @Giappo
+
+github_address=$1
+
+chmod +x /home/$USER/Rlibrary/
+
+echo "github_address = commandArgs(TRUE)" > Rsetup.R
+echo "path = getwd()" >> Rsetup.R
+echo "home_dir = substring(path,1,13)" >> Rsetup.R
+echo "lib_dir = file.path(home_dir, 'Rlibrary')" >> Rsetup.R
+echo "print(lib_dir)" >> Rsetup.R
+echo ".libPaths(new = lib_dir)" >> Rsetup.R
+echo "devtools::install_github(github_address)" >> Rsetup.R
+
+echo "#!/bin/bash" > Rsetup2
+echo "#SBATCH --time=00:59:00" >> Rsetup2
+echo "path='pwd'" >> Rsetup2
+echo "module load R/3.5.0-foss-2018a-X11-20180131" >> Rsetup2
+echo "Rscript Rsetup.R $github_address" >> Rsetup2
+echo "rm Rsetup.R" >> Rsetup2
+echo "rm Rsetup2" >> Rsetup2
+
+chmod +x Rsetup2
+./Rsetup2
